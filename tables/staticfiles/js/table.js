3c3f3f2d8be4c7e2288101930e51e9fca52e1025
CodiProgs <codi.progs@gmail.com>
2025-08-27 09:27:37 +0300

    update 6.12

--- tables/staticfiles/js/table.js
+++ tables/staticfiles/js/table.js
@@ -2090,8 +2090,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	function resizeCharts() {
 		const statsChart = document.getElementById('statsChart')
 		const profitChart = document.getElementById('profitChart')
-
-		if (!statsChart && !profitChart) return
+		console.log(statsChart, profitChart)
+		if (!statsChart || !profitChart) return
+		console.log('resizeCharts')
 		const w = statsChart.parentElement.offsetWidth
 		let h = 264
 		let profitW = 40
